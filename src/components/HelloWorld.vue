23848c326c6fcc94f2a24f93cd115ace45b57d98
wb-xsh357744 <wb-xsh357744@alibaba-inc.com>
2018-07-09 15:13:51 +0800

    my_weex_demo_first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+Vue.use({
+  app
+})
+</script>
